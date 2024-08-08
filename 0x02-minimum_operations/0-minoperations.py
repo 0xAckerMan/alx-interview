8e38d96a0f5bbcaa21ff851dc5aa7dbbfe981b7a
0xAckerMan <joelkores1@gmail.com>
2024-08-08 22:25:10 +0300

    0x02. Minimum Operationswa

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,16 +9,15 @@ exactly n H characters in the file
 
 def minOperations(n):
     """
-    will return minumum number of ops needed
+    returns the minimum no of operations needed
     """
+    if not isinstance(n, int) or n <= 1:
+        return 0
     num = 0
     i = 2
-    if type(n) is not int or n <= 1:
-        return 0
-    while n != 1:
-        if n % 1 == 0:
-            n = n / i
-            num = num + 1
-        else:
-            i = i + 1
+    while n > 1:
+        while n % i == 0:
+            num += i
+            n //= i
+        i += 1
     return num
